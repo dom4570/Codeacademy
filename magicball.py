19f7da6ed508067d3d85c1bf5a6dd40d0fe7d1c9
dom4570 <70895650+dom4570@users.noreply.github.com>
2023-04-04 22:12:17 +0100

    Create magicball.py

---
+++ magicball.py
@@ -0,0 +1,38 @@
+import random
+
+name = input("name:")
+question = "is this real life?"
+
+
+if name == "":
+  print("Question: " + question)
+else:
+  print(name + " asks: "+ question)
+# print(f"{name} {question}")
+
+answer = ""
+
+random_number = random.randint(1,9)
+
+if random_number == 1:
+  answer = "Yes - definitely"
+elif random_number == 2:
+  answer = "It is decidedly so"
+elif random_number == 3:
+  answer = "Without a doubt"
+elif random_number == 4:
+  answer = "Reply hazy, try again"
+elif random_number == 5:
+  answer = "Better not tell you now"
+elif random_number == 6:
+  answer = "My sources say no"
+elif random_number == 7:
+  answer = "Outlook not so good"
+elif random_number == 8:
+  answer = "Very doubtful"
+elif random_number == 9:
+  answer = "Ask again later"
+else:
+  answer = "Error"
+
+print(f"Magic 8-Ball's answer: {answer}")
